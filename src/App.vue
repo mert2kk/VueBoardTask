a09a5af672dcc2f3ca358c575be02794dada2671
mert2kk <mertkadirkayan@gmail.com>
2025-07-09 20:51:56 +0300

    fixes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <header>
-    <div class="bg-red-200"><router-view /></div>
+    <div class="bg-primary"><router-view /></div>
     <div class="p-6 bg-gray-100 min-h-screen">
       <h1 class="text-2xl font-bold text-gray-800 mb-4">
         Tailwind + PrimeVue Test
       </h1>
-      <Button label="Kaydet" class="p-button-success mr-2" />
+      <Button label="Submit" class="bg-primary mr-2" />
       <Button label="Sil" class="p-button-danger" />
     </div>
   </header>
--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,19 @@ import App from './App.vue'
 
 import PrimeVue from 'primevue/config';
 
-import './tailwind.css'
-
+import Aura from '@primevue/themes/aura';
 import 'primeicons/primeicons.css'
 import router from './router'
 
+import './tailwind.css'
 
 const app = createApp(App);
 app.use(router)
 
 app.use(PrimeVue, {
-
+    theme: {
+        preset: Aura
+    }
 });
 
 app.mount('#app')
